f94c207ba53785bf6e5a9cb1118f1424ccd68952
Mau Camargo <52770775+camargomau@users.noreply.github.com>
2024-04-24 19:13:12 -0600

    Develop post layout

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function updateNav() {
-        const navItems = document.querySelectorAll('.nav-item');
+// document.addEventListener("DOMContentLoaded", function() {
+//     function updateNav() {
+//         const navItems = document.querySelectorAll('.nav-item');
 
-        if (window.innerWidth <= 1000) {
-            navItems.forEach(item => item.style.fontSize = "0"); // Hide text
-        } else {
-            navItems.forEach(item => item.style.fontSize = ""); // Show text
-        }
-    }
+//         if (window.innerWidth <= 1000) {
+//             navItems.forEach(item => item.style.fontSize = "0"); // Hide text
+//         } else {
+//             navItems.forEach(item => item.style.fontSize = ""); // Show text
+//         }
+//     }
 
-    window.addEventListener('resize', updateNav);
-    updateNav();
-});
+//     window.addEventListener('resize', updateNav);
+//     updateNav();
+// });
