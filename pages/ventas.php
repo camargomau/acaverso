2ead9f783b8599a4864fe2f9248762e974b5ab40
camargomau <52770775+camargomau@users.noreply.github.com>
2024-05-16 19:15:26 -0600

    Update ventas.php for dynamic content

---
+++ pages/ventas.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acaverso</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="../icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../icons/favicon-16x16.png">
+
+    <link rel="stylesheet" href="../auxiliary/styles.css">
+    <script src="../auxiliary/script.js"></script>
+</head>
+<body>
+    <div id="content">
+
+        <nav id="navbar">
+            <a id="encabezado" href="../index.html"><i id="encabezado-icon" data-lucide="book-a"></i><span id="encabezado-text">Acaverso</span></a>
+            <a href="../index.html" class="nav-item"><i data-lucide="home"></i>Inicio</a>
+            <a href="anuncios.html" class="nav-item"><i data-lucide="megaphone"></i>Anuncios</a>
+            <a href="estudiantes.html" class="nav-item"><i data-lucide="graduation-cap"></i>Estudiantes</a>
+            <a href="ventas.html" class="nav-item active"><i data-lucide="badge-dollar-sign"></i>Ventas</a>
+            <a href="citas.html" class="nav-item"><i data-lucide="message-circle-heart"></i>Citas</a>
+            <a href="profesores.html" class="nav-item"><i data-lucide="school"></i>Profesores</a>
+            <a href="acerca.php" class="nav-item about"><i data-lucide="info"></i>Acerca</a>
+        </nav>
+
+        <div id="posts-container">
+
+            <div class="post">
+                <?php
+                include "../auxiliary/config-local.php";
+
+                $sql = "SELECT * FROM ventas"; // Change your_table_name to the actual name of your table
+                $result = $db->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // Output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo '<div class="post">';
+                        echo '<div class="author">';
+                        echo '<img src="' . $row["author_img"] . '">';
+                        echo '<div class="author-text">';
+                        echo '<span><b>' . $row["author_author"] . '</b></span>';
+                        echo '<span class="date">' . $row["author_date"] . '</span>';
+                        echo '</div>';
+                        echo '</div>';
+                        echo '<div class="body">';
+                        echo '<h2>' . $row["body_h2"] . '</h2>';
+                        echo '<h3 class="price">' . $row["body_price"] . '</h3>';
+                        echo $row["body_p"];
+                        echo '<img src="' . $row["body_img"] . '">';
+                        echo '</div>';
+                        echo '</div>';
+                        echo '</div>';
+                    }
+                } else {
+                    echo "Ningún resultado.";
+                }
+                $db->close();
+                ?>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://unpkg.com/lucide@latest"></script>
+    <script>
+        lucide.createIcons();
+    </script>
+</body>
+</html>
