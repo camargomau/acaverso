47935adbae88f899fb8b0de6ddb43c8dc456eed8
camargomau <52770775+camargomau@users.noreply.github.com>
2024-05-16 21:57:02 -0600

    Fix remote connection for index

--- index.php
+++ index.php
@@ -33,7 +33,20 @@
             $environment = getenv('ACAVERSO_ENV');
 
             if ($environment === 'azure') {
-                include 'auxiliary/config-remote.php';
+                $db = mysqli_init();
+                mysqli_ssl_set($db, NULL, NULL, "auxiliary/DigiCertGlobalRootCA.pem", NULL, NULL);
+
+                $hostname = getenv("DB_HOST");
+                $username = getenv("DB_USER");
+                $password = getenv("DB_PASSWORD");
+                $database = getenv("DB_NAME");
+                $port = getenv("DB_PORT");
+
+                $db_connection = mysqli_real_connect($db, $hostname, $username, $password, $database, $port, MYSQLI_CLIENT_SSL);
+
+                if (!$db_connection) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
             } else {
                 include 'auxiliary/config-local.php';
             }
